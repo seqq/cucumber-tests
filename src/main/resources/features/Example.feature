1ce86a77235bbe4c6090c599dcbb2d850e5eb709
Mathew <arun.a.mathew@accenture.com>
2017-07-03 13:45:58 +0530

    Initial Commit

---
+++ src/main/resources/features/Example.feature
@@ -0,0 +1,10 @@
+Feature: Example feature
+
+  As a tester,
+  I would like to utilize cucumber,
+  So that I can create BDD style tests.
+
+  @ExampleFeature
+  Scenario: Google search, using selenium
+    Given I have navigated to google
+    Then the page title should be "Google"
---
+++ src/main/scala/steps/ExampleSteps.scala
@@ -0,0 +1,36 @@
+package steps
+
+import java.util.concurrent.TimeUnit
+import java.net.URL
+
+import cucumber.api.scala.{EN, ScalaDsl}
+import org.openqa.selenium.support.ui.{ExpectedConditions, FluentWait, Wait}
+import org.openqa.selenium.{By, WebDriver}
+import org.openqa.selenium.remote.DesiredCapabilities
+import org.openqa.selenium.remote.RemoteWebDriver
+import org.scalatest.Matchers
+
+class ExampleSteps extends ScalaDsl with EN {
+  private val log = LoggerFactory.getLogger(classOf[ExampleSteps])
+
+  val capability = DesiredCapabilities.chrome()
+  val driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capability)
+
+  val fluentWait: Wait[WebDriver] = new FluentWait[WebDriver](driver)
+    .withTimeout(6, TimeUnit.SECONDS)
+    .pollingEvery(1, TimeUnit.SECONDS)
+
+  Before { scenario =>
+    log.info("Before...")
+  }
+  After { scenario =>
+    log.info("...After")
+  }
+
+  Given("""^I have navigated to google$""") { () =>
+    driver.navigate().to("http://www.google.com")
+  }
+  Then("""^the page title should be "(.*?)"$""") { (title: String) =>
+    fluentWait.until(ExpectedConditions.titleIs(title))
+  }
+}
